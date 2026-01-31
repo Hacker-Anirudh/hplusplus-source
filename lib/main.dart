0a9e25186589b43bfacc0052fd74974a3d48529c
Anirudh Menon <thenasaplusit@proton.me>
2026-02-01 00:42:21 +0100

    a lot of shit

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:url_launcher/url_launcher.dart';
+import 'package:hplusplus/settings_screen.dart';
+import 'package:hplusplus/common.dart';
 
 void main() {
   runApp(const MyApp());
@@ -14,120 +15,194 @@ class MyApp extends StatefulWidget {
 
 class _MyAppState extends State<MyApp> {
   bool isDark = true;
-  static const searchEngine = 1; // TODO: Implement user choice and cookies.
-  static const Map<int, List> searchEngines = {1: ["DuckDuckGo", 'https://duckduckgo.com/?q='], 2: ["Google",'https://www.google.com/search?q='], 3: ["Bing", 'https://www.bing.com/search?q='], 4: ["Brave", 'https://search.brave.com/search?q='], 5: ["Ecosia", 'https://www.ecosia.org/search?q='], 6: ["Yahoo", 'https://search.yahoo.com/search?p='], 7: ["Startpage", 'https://www.startpage.com/sp/search?q=']};
-  static const graad = 2; //TODO: Add option to select this.
+  int _selectedIndex = 0;
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'H++',
 
-      theme: ThemeData(
-        useMaterial3: true,
-        brightness: isDark ? Brightness.dark : Brightness.light,
-        colorScheme: ColorScheme.fromSeed(
-          seedColor: isDark ? Colors.blueAccent : Colors.lightBlueAccent,
-          brightness: isDark ? Brightness.dark : Brightness.light,
-        ),
-      ),
+      theme: appTheme(),
 
       home: Scaffold(
         floatingActionButton: FloatingActionButton(
-          onPressed: () {
-            setState(() {
-                  isDark = !isDark;
-                });
-          },
+          onPressed: () {},
           child: Tooltip(
             message: 'Verander van kleurenmodus',
-            child: Icon(isDark ? Icons.wb_sunny_outlined : Icons.brightness_2_outlined),
+            child: IconButton(
+              isSelected: isDark,
+              onPressed: () {
+                setState(() {
+                  isDark = !isDark;
+                });
+              },
+              icon: const Icon(Icons.wb_sunny_outlined),
+              selectedIcon: const Icon(Icons.brightness_2_outlined),
+            ),
           ),
         ),
         appBar: AppBar(
-          title: const Text('H++ Startpagina', style: TextStyle(fontFamily: 'VT323', fontSize: 28)),
-                  actions: [
-          SizedBox(
-            width: 250,
-            child: TextField(
-              onSubmitted: (String value) {
-                _openlink(searchEngines[searchEngine]![1]! + value);
-              },
-              decoration: InputDecoration(
-                prefixIcon: Icon(Icons.search),
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(64.0),
-                ),
-                hintText: 'Tik een zoekterm in',
-                labelText: 'Zoeken met ${searchEngines[searchEngine]![0]!}',
+        title: const Text('H++ Startpagina', style: TextStyle(fontFamily: 'VT323', fontSize: 28)),
+                actions: [
+        SizedBox(
+          width: 250,
+          child: TextField(
+            onSubmitted: (String value) {
+              Common.openlink(searchEngines[searchEngine]![1] + value);
+            },
+            decoration: InputDecoration(
+              prefixIcon: Icon(Icons.search),
+              border: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(64.0),
               ),
+              hintText: 'Tik een zoekterm in',
+              labelText: searchEngines[searchEngine]![0],
             ),
           ),
-        ],
         ),
-        body: SingleChildScrollView(
-            child: Column(
-              mainAxisAlignment: .center,
-              children: [
-                Padding(padding:  EdgeInsets.all(24.0),
-                child: Text("Belangrijk", style: TextStyle(fontFamily: 'VT323', fontSize: 36)),
-              ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: ElevatedButton.icon(
-                        onPressed: () {
-                          _openlink("https://hhscholen.schoolware.be/webleerling/start.html");
-                        },
-                        icon: Icon(Icons.calendar_today_rounded, size: 24),
-                        label: Text('Schoolware', style: TextStyle(fontSize: 18)),
-                        style: ElevatedButton.styleFrom(minimumSize: Size(280, 96)),
-                      ),
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: ElevatedButton.icon(
-                        onPressed: () {
-                          _openlink("https://outlook.cloud.microsoft/mail/");
-                        },
-                        icon: Icon(Icons.email_rounded, size: 24),
-                        label: Text('Outlook', style: TextStyle(fontSize: 18)),
-                        style: ElevatedButton.styleFrom(minimumSize: Size(280, 96)),
-                      ),
-                    ),
-                    Padding(padding:  EdgeInsets.all(8.0),
-                    child: ElevatedButton.icon(
-                      onPressed: () {
-                        _openlink("https://teams.microsoft.com/v2/");
-                      },
-                      icon: Icon(Icons.groups_rounded, size: 24),
-                      label: Text('Teams', style: TextStyle(fontSize: 18)),
-                      style: ElevatedButton.styleFrom(minimumSize: Size(280, 96)),     
-                    )
-                    ),
-                    Padding(padding:  EdgeInsets.all(8.0),
-                    child: ElevatedButton.icon(
-                      onPressed: () {
-                        _openlink("https://hhh.exxeda.com/overzicht/vervangingen?graad=$graad");
-                      },
-                      icon: Icon(Icons.question_mark_rounded, size: 24),
-                      label: Text('Afwezigen en vervangingen', style: TextStyle(fontSize: 18)),
-                      style: ElevatedButton.styleFrom(minimumSize: Size(280, 96)),     
-                    )
-                    ),
-                  ],
-                )
+      ],
+      ),
+        body: Row(
+          children: [
+            NavigationRail(
+              selectedIndex: _selectedIndex,
+              onDestinationSelected: (int index) {
+                setState(() {
+                  _selectedIndex = index;
+                });
+              },
+              labelType: NavigationRailLabelType.all,
+              destinations: const [
+                NavigationRailDestination(
+                  icon: Icon(Icons.home),
+                  label: Text('Start'),
+                ),
+                NavigationRailDestination(icon: Icon(Icons.settings_rounded), label: Text('Instellingen')),
+                NavigationRailDestination(icon: Icon(Icons.grid_3x3_rounded), label: Text('Lessenrooster')),
               ],
-          )
-          )  
+            ),
+            Expanded(
+              child: _selectedIndex == 0
+                  ? homeScreen()
+                  : _selectedIndex == 1
+                      ? const SettingsScreen()
+                      : lessenroosterScreen(),
+            ),
+          ],
+        ),
       ),
     );
   }
-}
 
-void _openlink(String url) {
-  final Uri _url = Uri.parse(url);
-  launchUrl(_url);
+  ThemeData appTheme() {
+    return ThemeData(
+      useMaterial3: true,
+      brightness: isDark ? Brightness.dark : Brightness.light,
+      colorScheme: ColorScheme.fromSeed(
+        seedColor: isDark ? Colors.blueAccent : Colors.lightBlueAccent,
+        brightness: isDark ? Brightness.dark : Brightness.light,
+      ),
+    );
+  }
+
+  Scaffold homeScreen() {
+    return Scaffold(
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          setState(() {
+                isDark = !isDark;
+              });
+        },
+        child: Tooltip(
+          message: 'Verander van kleurenmodus',
+          child: Icon(isDark ? Icons.wb_sunny_outlined : Icons.brightness_2_outlined),
+        ),
+      ),
+      body: SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: .center,
+            children: [
+              Padding(padding:  EdgeInsets.all(24.0),
+              child: Text('Belangrijk', style: TextStyle(fontFamily: 'VT323', fontSize: 36)),
+            ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: ElevatedButton.icon(
+                      onPressed: () {
+                        Common.openlink('https://hhscholen.schoolware.be/webleerling/start.html');
+                      },
+                      icon: Icon(Icons.calendar_today_rounded, size: 24),
+                      label: Text('Schoolware', style: TextStyle(fontSize: 18)),
+                      style: ElevatedButton.styleFrom(minimumSize: Size(280, 96)),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: ElevatedButton.icon(
+                      onPressed: () {
+                        Common.openlink('https://outlook.cloud.microsoft/mail/');
+                      },
+                      icon: Icon(Icons.email_rounded, size: 24),
+                      label: Text('Outlook', style: TextStyle(fontSize: 18)),
+                      style: ElevatedButton.styleFrom(minimumSize: Size(280, 96)),
+                    ),
+                  ),
+                  Padding(padding:  EdgeInsets.all(8.0),
+                  child: ElevatedButton.icon(
+                    onPressed: () {
+                      Common.openlink('https://teams.microsoft.com/v2/');
+                    },
+                    icon: Icon(Icons.groups_rounded, size: 24),
+                    label: Text('Teams', style: TextStyle(fontSize: 18)),
+                    style: ElevatedButton.styleFrom(minimumSize: Size(280, 96)),     
+                  )
+                  ),
+                  Padding(padding:  EdgeInsets.all(8.0),
+                  child: ElevatedButton.icon(
+                    onPressed: () {
+                      int _graad = graad.toInt();
+                      Common.openlink('https://hhh.exxeda.com/overzicht/vervangingen?graad=$_graad');
+                    },
+                    icon: Icon(Icons.question_mark_rounded, size: 24),
+                    label: Text('Afwezigen en vervangingen', style: TextStyle(fontSize: 18)),
+                    style: ElevatedButton.styleFrom(minimumSize: Size(280, 96)),     
+                  )
+                  ),
+                ],
+              ),
+              Padding(padding:  EdgeInsets.all(24.0),
+              child: Text('Info', style: TextStyle(fontFamily: 'VT323', fontSize: 36)),
+            ),
+            Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: ElevatedButton.icon(
+                      onPressed: () {
+                        setState(() {
+                          _selectedIndex = 2;
+                        });
+                      },
+                      icon: Icon(Icons.grid_3x3_rounded, size: 24),
+                      label: Text('Lessenrooster', style: TextStyle(fontSize: 18)),
+                      style: ElevatedButton.styleFrom(minimumSize: Size(280, 96)),
+                    ),
+                  ),
+                ],
+              ),
+            ],
+        )
+        )  
+    );
+  }
+  
+  Scaffold lessenroosterScreen() {
+    return Scaffold(
+      body: Image.network('https://llnroosters.hhscholen.be/klassen/ed${classMap[klas]}p00001s3fffffffffffffff_${klas.toLowerCase()}_vs.png')
+    );
+  }
 }
